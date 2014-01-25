6355493a3da84675fe412846e6dc8554bc129fab
Rohan Das <das.rohan1993@gmail.com>
2014-01-25 08:27:09 -0500

    updated file saving

--- saveapps/saver/views.py
+++ saveapps/saver/views.py
@@ -21,22 +21,21 @@ def SaveContent(request):
 
 def make_file(request, file_type):
 	content = request.POST[file_type]
-	to_create_name = new_file_name(request, file_type)
+	to_create_name = get_new_file_name(request, file_type)
 	to_create = open('static/saved/' + to_create_name, 'w')
 	to_create.write(content)
 	to_create.close()
 	return to_create_name
 
-def new_file_name(request, file_type):
+def get_new_file_name(request, file_type):
 	index_tracker = open('indices.txt', 'r+')
-	index = int(index_tracker.read())
-	for count in range (index + 1):
-		name = file_type + str(count)
-		if not os.path.exists(name):
-			return name
-	new_index = index + 1
+	index = index_tracker.read()
+	
+	new_file_name = file_type + str(index) + "." + file_type
+	
+	new_index = int(index) + 1
 	index_tracker.seek(0)
 	index_tracker.write(str(new_index))
 	index_tracker.truncate()
 	index_tracker.close()
-	return new_file_name(request, file_type)
+	return new_file_name
