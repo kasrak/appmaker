d7cbc34901fdfe16f8c0c44b666db05be04e3407
Kasra Kyanzadeh <me@kasrak.com>
2014-01-24 21:18:53 -0500

    Drag elements

--- src/app.js
+++ src/app.js
@@ -4,11 +4,10 @@ $(function() {
 
     $draggables.forEach(function(draggable) {
         draggable.addEventListener('dragstart', function(e) {
-            console.log('drag start');
+            e.dataTransfer.setData('text/plain', $(e.target).data('element-type'));
         }, false);
 
         draggable.addEventListener('dragend', function(e) {
-            console.log('drag end');
         }, false);
     });
 
@@ -21,6 +20,33 @@ $(function() {
     }, false);
 
     $canvas[0].addEventListener('drop', function(e) {
-        console.log('drop');
+        if (e.stopPropagation) {
+            e.stopPropagation();
+        }
+
+        var elementType = e.dataTransfer.getData('text/plain');
+        var element;
+
+        switch(elementType) {
+            case 'button':
+                element = $('<button>Button</button>');
+                break;
+            case 'label':
+                element = $('<div class="element-label">Label...</div>');
+                break;
+            case 'image':
+                element = $('<img width="100" height="100">');
+                break;
+            case 'text':
+                element = $('<input type="text">');
+                break;
+        }
+
+        element.addClass('element');
+        element.css({ top: e.offsetY, left: e.offsetX });
+
+        $canvas.append(element);
+
+        return false;
     });
 });
