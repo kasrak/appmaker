91397c90d80debb3f2592c627e0acfe9a30fc8ca
Kasra Kyanzadeh <me@kasrak.com>
2015-01-14 23:40:14 -0500

    Clean up settings.py

--- saveapps/saveapps/settings.py
+++ saveapps/saveapps/settings.py
@@ -30,19 +30,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 ROOT_URLCONF = 'saveapps.urls'
